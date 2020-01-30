cb5a8c20faaed972e8fa15f9754c9138396dc262
Nikolett Palyi <nikpalyi@gmail.com>
2020-01-30 15:15:28 +0000

    Updates

---
+++ precache-manifest.1703646d242ea1a7d02b3a0bd3907018.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "356fce81ffa23edd9e79",
+    "url": "/commenting-app/static/js/runtime~main.356fce81.js"
+  },
+  {
+    "revision": "f62277585807f0a8fbe5",
+    "url": "/commenting-app/static/js/main.f6227758.chunk.js"
+  },
+  {
+    "revision": "f3b84ad1a4a60d245526",
+    "url": "/commenting-app/static/js/1.f3b84ad1.chunk.js"
+  },
+  {
+    "revision": "f62277585807f0a8fbe5",
+    "url": "/commenting-app/static/css/main.53126507.chunk.css"
+  },
+  {
+    "revision": "74ede784ead50757bb7d5085fc0f4092",
+    "url": "/commenting-app/index.html"
+  }
+];
